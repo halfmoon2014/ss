ba30127c15bf65ca452ab2675e4eb689b55a381d
halfmoon2014 <mrzhangmh@gmail.com>
2018-07-18 14:12:29 +0800

    2

--- Library/DTO/PageCmContent.cs
+++ Library/DTO/PageCmContent.cs
@@ -15,26 +15,81 @@ namespace DTO
         /// <summary>
         /// 业务详情对应的尺码数据
         /// </summary>
-        public DataTable CmDetailsData { get => cmDetailsData; set => cmDetailsData = value; }
+        public DataTable CmDetailsData
+        {
+            get
+            {
+                return cmDetailsData;
+            }
+
+            set
+            {
+                cmDetailsData = value;
+            }
+        }
 
         /// <summary>
         /// 尺码标题
         /// </summary>
-        public DataTable CmHeadlineData { get => cmHeadlineData; set => cmHeadlineData = value; }
+        public DataTable CmHeadlineData
+        {
+            get
+            {
+                return cmHeadlineData;
+            }
+
+            set
+            {
+                cmHeadlineData = value;
+            }
+        }
 
         /// <summary>
         /// 详情与尺码标题的关联
         /// </summary>
-        public string MasterCmRelation { get => masterCmRelation; set => masterCmRelation = value; }
+        public string MasterCmRelation
+        {
+            get
+            {
+                return masterCmRelation;
+            }
+
+            set
+            {
+                masterCmRelation = value;
+            }
+        }
 
         /// <summary>
         /// 详情和尺码的关联
         /// </summary>
-        public string MasterSlaveKey { get => masterSlaveKey; set => masterSlaveKey = value; }
+        public string MasterSlaveKey
+        {
+            get
+            {
+                return masterSlaveKey;
+            }
+
+            set
+            {
+                masterSlaveKey = value;
+            }
+        }
 
         /// <summary>
         /// 尺码与尺码标题的关联
         /// </summary>
-        public string DetailCmRelation { get => detailCmRelation; set => detailCmRelation = value; }
+        public string DetailCmRelation
+        {
+            get
+            {
+                return detailCmRelation;
+            }
+
+            set
+            {
+                detailCmRelation = value;
+            }
+        }
     }
 }
--- Library/DTO/PageContent.cs
+++ Library/DTO/PageContent.cs
@@ -13,31 +13,97 @@ namespace DTO
         /// <summary>
         /// 记录总数
         /// </summary>
-        public int RecordCount { get => recordCount; set => recordCount = value; }
+        public int RecordCount
+        {
+            get
+            {
+                return recordCount;
+            }
+
+            set
+            {
+                recordCount = value;
+            }
+        }
 
         /// <summary>
         /// 业务详情各项的数据类型
         /// </summary>
-        public DataTable ColumnDataType { get => columnDataType; set => columnDataType = value; }
+        public DataTable ColumnDataType
+        {
+            get
+            {
+                return columnDataType;
+            }
+
+            set
+            {
+                columnDataType = value;
+            }
+        }
 
         /// <summary>
         /// 完整的业务详情
         /// </summary>
-        public DataTable TotalDetailsData { get => totalDetailsData; set => totalDetailsData = value; }
+        public DataTable TotalDetailsData
+        {
+            get
+            {
+                return totalDetailsData;
+            }
+
+            set
+            {
+                totalDetailsData = value;
+            }
+        }
 
         /// <summary>
         /// 详情数据SQL
         /// </summary>
-        public string DetailsSql { get => detailsSql; set => detailsSql = value; }
+        public string DetailsSql
+        {
+            get
+            {
+                return detailsSql;
+            }
+
+            set
+            {
+                detailsSql = value;
+            }
+        }
 
         /// <summary>
         /// 当前页要显示的业务详情
         /// </summary>
-        public DataTable DetailsData { get => detailsData; set => detailsData = value; }
+        public DataTable DetailsData
+        {
+            get
+            {
+                return detailsData;
+            }
+
+            set
+            {
+                detailsData = value;
+            }
+        }
 
         /// <summary>
         /// 业务详情对应的尺码数据
         /// </summary>
-        public DataTable CmDetailsData { get => cmDetailsData; set => cmDetailsData = value; }
+        public DataTable CmDetailsData
+        {
+            get
+            {
+                return cmDetailsData;
+            }
+
+            set
+            {
+                cmDetailsData = value;
+            }
+        }
     }
 }
